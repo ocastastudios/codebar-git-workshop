5229d604673ee1272cd8b44fa7f480790c3e1f39
Cassie Evans <cassie.evans@netnatives.co.uk>
2018-05-18 21:26:24 +0100

    template website ready for the workshop!

---
+++ js/nav.js
@@ -0,0 +1,6 @@
+aObj = document.getElementById('nav').getElementsByTagName('a');
+for (i = 0; i < aObj.length; i++) {
+    if (document.location.href.indexOf(aObj[i].href) >= 0) {
+        aObj[i].className = 'active';
+    }
+}
